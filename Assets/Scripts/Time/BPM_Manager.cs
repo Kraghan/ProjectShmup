851148169c9f837ab99e1d0f79b4d0643628d0a9
YoueG <Youe.Graillot@etu.udamail.fr>
2017-12-15 16:59:21 +0100

    + BPM

---
+++ Assets/Scripts/Time/BPM_Manager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BPM_Manager : MonoBehaviour
+{
+	static UnityAction m_syncList;
+
+	public static UnityAction SyncedAction
+	{
+		set
+		{
+			m_syncList = value;
+		}
+
+		get
+		{
+			return m_syncList;
+		}
+	}
+
+	[SerializeField, Range(100,200)]
+	int m_BPM = 128;
+
+	float m_beatDuration;
+	float m_timeSinceLastBeat = 0;
+
+	void Start ()
+	{
+		m_beatDuration = 60f / m_BPM;
+		print(m_beatDuration);
+	}
+	
+	void Update ()
+	{
+		m_timeSinceLastBeat += Time.deltaTime;
+
+		// Beat
+		if(m_timeSinceLastBeat > m_beatDuration)
+		{
+			m_timeSinceLastBeat = 0;
+			m_syncList.Invoke();
+		}
+	}
+}
