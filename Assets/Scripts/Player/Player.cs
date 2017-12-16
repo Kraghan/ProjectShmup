0259df06a6fcaec62d45ce28cf16b0b92374ee3b
YoueG <Youe.Graillot@etu.udamail.fr>
2017-12-16 12:35:11 +0100

    WIP

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
-public class Player : MonoBehaviour
+public class Player : Killable
 {
     #region Attributes
     #endregion
