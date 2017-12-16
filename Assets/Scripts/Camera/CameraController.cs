b9ba01ee7d4d8a65cc271863e4a78412efbe2bb7
Unknown <madjojo70@live.fr>
2017-12-16 14:43:52 +0100

    Merge + Add spawn and camera in Main

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -10,15 +10,22 @@ public class CameraController : MonoBehaviour {
 
     #endregion
 
+    #region Monobehaviour
     // Use this for initialization
     void Start ()
     {
         
     }
-	
+
 	// Update is called once per frame
 	void Update () {
         Vector3 pos = transform.position;
         transform.position = new Vector3(pos.x + scrollSpeed * Time.deltaTime, pos.y, pos.z);
 	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+           
+    }
+    #endregion
 }
