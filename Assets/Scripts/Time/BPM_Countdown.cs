680bac55bc18294ae15806df5ab5c5aec577d3ff
YOUE\Youe <youe.graillot@gmail.com>
2018-01-18 17:29:32 +0100

    CountDown component

---
+++ Assets/Scripts/Time/BPM_Countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BPM_Countdown : BPM_Events
+{
+	[SerializeField]
+	UnityEvent m_OnEndBeat;
+	[SerializeField]
+	int m_count;
+
+	public override void OnBeat()
+	{
+		if(m_count != 0)
+		{
+			m_OnBeat.Invoke();
+
+			m_count--;
+			
+			if(m_count == 0)
+				m_OnEndBeat.Invoke();
+		}
+	}
+}
