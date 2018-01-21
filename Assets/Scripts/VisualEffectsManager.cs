d5888471c51aabdb3d7a71685a128adca8757347
YOUE\Youe <youe.graillot@gmail.com>
2018-01-21 15:24:38 +0100

    + Visual FX manager

---
+++ Assets/Scripts/VisualEffectsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualEffectsManager : MonoBehaviour {
+
+	[SerializeField]
+	IntVariable m_state;
+
+	[SerializeField]
+	float m_speed;
+
+	[SerializeField]
+	Transform[] m_serial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int tID = 0; tID < m_serial.Length; tID++)
+		{
+			m_serial[tID].localPosition = Vector3.MoveTowards(m_serial[tID].localPosition, tID+1 == m_state.value ? Vector2.zero : Vector2.one, m_speed);
+		}
+	}
+}
