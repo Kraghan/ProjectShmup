629e6a4f0636ec5fde0cb7f4eaa6a047f55f8d03
YOUE\Youe <youe.graillot@gmail.com>
2018-01-22 11:07:10 +0100

    Reintegration LD

---
+++ Assets/Scripts/Pools.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pools : GameJolt.API.Core.MonoSingleton<Pools>
+{
+	public Transform Bullets, Enemies;
+}
