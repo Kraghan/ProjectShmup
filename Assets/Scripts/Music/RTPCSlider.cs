eaff4b490122047a2ba2b5921968353b054623d2
YOUE\Youe <youe.graillot@gmail.com>
2018-01-25 18:20:21 +0100

    + Sliders

---
+++ Assets/Scripts/Music/RTPCSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTPCSlider : MonoBehaviour {
+
+	[SerializeField]
+	string m_rtpc;
+
+	public void CallRTPC(float value)
+	{
+		AkSoundEngine.SetRTPCValue(m_rtpc, value*100);
+	}
+}
